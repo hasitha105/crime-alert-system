76df869b5e846f4ab658438fea932daf9503b9e9
hemalatha <hemalatha.23bce20119@vitapstudent.ac.in>
2025-09-07 14:11:30 +0530

    Added render-build.sh and prepared for deployment

--- render-build.sh
+++ render-build.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-# Upgrade pip inside Render's virtual environment
-/opt/render/project/src/.venv/bin/pip install --upgrade pip
-
-# Install dependencies inside the virtual environment
-/opt/render/project/src/.venv/bin/pip install -r requirements.txt
-
-# Download spaCy model inside the virtual environment
-/opt/render/project/src/.venv/bin/python -m spacy download en_core_web_sm
+pip install --upgrade pip
+pip install -r requirements.txt
+python -m spacy download en_core_web_sm
